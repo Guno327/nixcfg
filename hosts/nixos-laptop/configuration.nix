017faf93daab5fd848045384204bd30989490d1a
Guno327 <gunnarhovik@outlook.com>
2024-12-20 21:48:08 -0700

    limited bootloader entries

--- hosts/nixos-laptop/configuration.nix
+++ hosts/nixos-laptop/configuration.nix
@@ -16,6 +16,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
 
   networking.hostName = "nixos-laptop";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
