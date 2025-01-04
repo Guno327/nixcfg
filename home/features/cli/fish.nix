6d86b8bd8c3220735a2759b0732900f65aa6ba7c
Guno327 <gunnarhovik@outlook.com>
2025-01-04 01:28:43 -0700

    moved hyprland start on tty1 to hyprland config

--- home/features/cli/fish.nix
+++ home/features/cli/fish.nix
@@ -10,10 +10,6 @@ in {
       set -x NIX_PATH nixpkgs=channel:nixos-unstable
       set -x NIX_LOG info
       set -x TERMINAL kitty
-
-	    if test (tty) = "/dev/tty1"
-        exec Hyprland &> /dev/null
-      end
     '';
     shellAbbrs = {
       ".." = "cd ..";
--- home/features/desktop/hyprland.nix
+++ home/features/desktop/hyprland.nix
@@ -27,6 +27,17 @@ in {
 
     programs.wofi.enable = true;
 
+    programs.fish.loginShellInit = ''
+      set -x NIX_PATH nixpkgs=channel:nixos-unstable
+      set -x NIX_LOG info
+      set -x TERMINAL kitty
+
+	    if test (tty) = "/dev/tty1"
+        exec Hyprland &> /dev/null
+      end
+    '';
+
+
     programs.hyprlock = {
       enable = true;
         
