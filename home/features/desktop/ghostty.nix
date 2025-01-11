ef00571077f737fce95765ea20001978b934863b
Guno327 <gunnarhovik@outlook.com>
2025-01-11 16:03:56 -0700

    switched to ghostty on desktop

--- home/features/desktop/ghostty.nix
+++ home/features/desktop/ghostty.nix
@@ -1,8 +1,6 @@
 {
   config,
   lib,
-  inputs,
-  pkgs,
   ...
 }:
 with lib; let
@@ -11,8 +9,17 @@ in {
   options.features.desktop.ghostty.enable = mkEnableOption "Configure ghostty";
 
   config = mkIf cfg.enable {
+    programs.ghostty = {
+      enable = true;
+      enableFishIntegration = true;
+      settings = {
+        font-family = "FiraCode Nerd Font";
+        font-size = 14;
+        theme = "Dracula";
+      };
+    };
     wayland.windowManager.hyprland.settings = {
-      bind = [ "$mainMod, RETURN, exec, ghostty -e fish -c 'exec fish'" ];
+      bind = [ "$mainMod, RETURN, exec, ghostty -e fish" ];
     };
   };
 }
