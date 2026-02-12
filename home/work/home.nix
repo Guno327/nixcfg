952d6e6d2f7de3f5a0d0ecc264f963ca30d564a1
guno327 <accounts@ghov.net>
2026-02-12 17:37:26 -0500

    need to set generic linux flag

--- home/work/home.nix
+++ home/work/home.nix
@@ -8,6 +8,7 @@
     homeDirectory = lib.mkDefault "/home/${config.home.username}";
 
     stateVersion = "24.05"; # DO NOT CHANGE
+    targets.genericLinux.enable = true;
 
     sessionVariables = {
       EDITOR = "nix run github:guno327/nvf-flake";
