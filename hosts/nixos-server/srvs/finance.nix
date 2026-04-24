c48e898ec97a27b16c38e209ea080e559026b120
guno327 <accounts@ghov.net>
2026-04-23 20:47:06 -0400

    enabled OIDC for actual budget

--- hosts/nixos-server/srvs/finance.nix
+++ hosts/nixos-server/srvs/finance.nix
@@ -31,12 +31,41 @@ in
       };
     };
 
+    users = {
+      users.actual = {
+        uid = 61469;
+        name = "actual";
+        isSystemUser = true;
+        home = "/data/actual";
+        group = "actual";
+      };
+      groups.actual = {
+        name = "actual";
+        gid = 61469;
+      };
+    };
+
+    sops.secrets.actual = {
+      owner = "actual";
+      mode = "0600";
+    };
+
     services.actual = {
       enable = true;
+      user = "actual";
+      group = "actual";
       settings = {
         dataDir = "/data/actual";
         hostname = "127.0.0.1";
         port = 3000;
+        trustedProxies = [ "127.0.0.1/32" ];
+
+        openId = {
+          discoveryURL = "https://auth.ghov.net/application/o/actual-budget/.well-known/openid-configuration";
+          client_id = "ActualBudget";
+          client_secret._secret = config.sops.secrets.actual.path;
+          server_hostname = "https://finance.ghov.net";
+        };
       };
     };
   };
