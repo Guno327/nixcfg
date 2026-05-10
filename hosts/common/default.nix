1ce75a6fb14b9776ca0b3672e61382a69cb23a9b
guno327 <accounts@ghov.net>
2026-05-09 21:30:42 -0400

    moved sytlix to own file

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -12,6 +12,7 @@
     ./users
     ./sops.nix
     ./nebula.nix
+    ./stylix.nix
   ];
 
   home-manager = {
@@ -123,40 +124,4 @@
     ];
   };
 
-  stylix = {
-    enable = true;
-    autoEnable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    polarity = "dark";
-
-    cursor = {
-      package = pkgs.stable.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 16;
-    };
-
-    fonts = {
-      serif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Serif";
-      };
-
-      sansSerif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans";
-      };
-
-      monospace = {
-        package = pkgs.fira-code;
-        name = "Fira Code";
-      };
-
-      sizes = {
-        applications = 12;
-        desktop = 12;
-        terminal = 14;
-        popups = 12;
-      };
-    };
-  };
 }
